82bbbe29ac2bc2b3cff2bc9d96b9dac9c7105cbf
MAHMUDUL HASAN SAKIB <157484516+SAKIB-2024@users.noreply.github.com>
2025-10-31 00:42:28 +0600

    Add Newton-Raphson method implementation
    Implement Newton-Raphson method for root finding.

---
+++ Root_Finding_Methods/Newton_Raphson_Method.py
@@ -0,0 +1,18 @@
+import math
+def f1(x):
+    return math.exp(-x) - x
+def f2(x):
+    return -math.exp(-x) - 1
+
+prev = 0
+tolarance = 0.001
+
+for i in range(1, 11):
+    x = prev - f1(prev) / f2(prev)
+    err = abs(x - prev)
+    prev = x
+    print(f"Iteration num : {i}  current_root: {x:.5f}  Abs_err: {err:.8f}")
+
+    if err < tolarance:
+        break
+    
